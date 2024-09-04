a60d52c47d57645b41df75abd732d9aae2804593
edwin-monash <ejos0008@student.monash.edu>
2024-09-04 20:30:31 +1000

    installed sustainScoreMap in project

--- quickstartproject/settings.py
+++ quickstartproject/settings.py
@@ -32,6 +32,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # my_apps
+    'sustainScoreMap',
+
     "whitenoise.runserver_nostatic",
     'django.contrib.admin',
     'django.contrib.auth',
