7af8e9de129ce277a7e7c472024f91038342cba0
edwin-monash <ejos0008@student.monash.edu>
2024-09-12 15:58:30 +1000

    mapbox_api_key passed to template

--- sustainScoreMap/views.py
+++ sustainScoreMap/views.py
@@ -6,6 +6,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from .forms import UserInputForm
 from .ml_model import score_model
+from django.conf import settings 
 
 def index(request):
     if request.method == 'POST':
@@ -23,16 +24,17 @@ def index(request):
             print(user_input)
 
             # Call the ML model and get the GeoJSON file
-            geojson_file = score_model(user_input)
+            geojson_url = score_model(user_input)
 
             print("I have recieved an output from the ML_model")
 
-            print(f"Here is where my file is stored {geojson_file}")
+            print(f"Here is where my file is stored {geojson_url}")
 
             # Pass the form and GeoJSON path back to the template
             return render(request, 'sustainScoreMap/sustainscore.html', {
                 'form': form,
-                'geojson_url': geojson_file
+                'geojson_url': geojson_url,
+                'mapbox_api_key': settings.MAPBOX_API_KEY
             })
     else:
         form = UserInputForm()
