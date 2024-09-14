b5c177fe8cb1657e75bac9752c67a7afbed87d96
edwin-monash <ejos0008@student.monash.edu>
2024-09-14 17:41:13 +1000

    login protection

--- compareSuburbsMap/views.py
+++ compareSuburbsMap/views.py
@@ -1,9 +1,9 @@
-# Create your views here.
-
 # compareSuburbsMap/views.py
 
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def index(request):
     return render(request, 'compareSuburbsMap/compare.html')
