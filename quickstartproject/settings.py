85d9fb8c9fda37633d182199de0b10af780527d2
edwin-monash <ejos0008@student.monash.edu>
2024-09-14 18:24:06 +1000

    login url modified

--- quickstartproject/settings.py
+++ quickstartproject/settings.py
@@ -74,7 +74,7 @@ AZURE_CONTAINER_URL = f"{AZURE_ACCOUNT_URL}/{AZURE_CONTAINER}"
 # Fetch the Mapbox API key from environment variables
 MAPBOX_API_KEY = os.getenv('MAPBOX_API_KEY')
 
-LOGIN_URL = 'login'  # This matches the name of your login URL
+LOGIN_URL = 'login/'  # This matches the name of your login URL
 LOGIN_REDIRECT_URL = '/'  # Where to redirect after successful login (optional)
 
 
