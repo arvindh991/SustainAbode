8d94c865d55122b3ff80bcab8852ed25eabd6a79
Raghavan Arvindh <rarv0001@student.monash.edu>
2024-09-26 17:32:38 +1000

    new views

--- quickstartproject/views.py
+++ quickstartproject/views.py
@@ -5,5 +5,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+# Landing page view
+def landing_page(request):
+    return render(request, 'landing_page.html')
+
 def index(request):
     return render(request, 'compareSuburbsMap/compare.html')
