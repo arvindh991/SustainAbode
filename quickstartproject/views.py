5021a1804ab4470620405b757bdcd25718729c49
Raghavan Arvindh <rarv0001@student.monash.edu>
2024-09-26 17:00:59 +1000

    changed views

--- quickstartproject/views.py
+++ quickstartproject/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def landing_page(request):
     return render(request, 'landing_page.html')
+
+    def index(request):
+    return render(request, 'compareSuburbsMap/compare.html')
