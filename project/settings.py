a2699fcfd72393115fb25b3009f47a3e3c449c5a
edwin-monash <ejos0008@student.monash.edu>
2024-10-15 08:10:30 +1100

    allowed host & CSRF

--- project/settings.py
+++ project/settings.py
@@ -13,9 +13,9 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else ['sustainabodeau-e2c9bec8e3hre8f0.australiasoutheast-01.azurewebsites.net', 'sustainabode.xyz', '127.0.0.1', 'http://20.70.176.88/']
+ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else ['sustainabodeau-e2c9bec8e3hre8f0.australiasoutheast-01.azurewebsites.net', 'sustainabode.xyz', '127.0.0.1', '20.70.176.88']
 
-CSRF_TRUSTED_ORIGINS = ['https://sustainabodeau-e2c9bec8e3hre8f0.australiasoutheast-01.azurewebsites.net', 'https://sustainabode.xyz', 'http://20.70.176.88/']
+CSRF_TRUSTED_ORIGINS = ['https://sustainabodeau-e2c9bec8e3hre8f0.australiasoutheast-01.azurewebsites.net', 'https://sustainabode.xyz', 'http://20.70.176.88']
 
 # Application definition
 
